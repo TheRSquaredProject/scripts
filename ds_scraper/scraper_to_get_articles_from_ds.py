dd11fa6eb056fb5503b23b5e3b80609194be624e
Azmain Amin <Azmain Amin>
2018-07-21 23:52:14 -0400

    Added docstring

--- ds_scraper/scraper_to_get_articles_from_ds.py
+++ ds_scraper/scraper_to_get_articles_from_ds.py
@@ -1,21 +1,34 @@
 import os
 import time as pytime
 import re
-import sys
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from datetime import date, timedelta
 
 base = "https://www.thedailystar.net/"
-numOfDaysToScrape = 2
+numOfDaysToScrape = 8
+
 
 class Section:
+    """ 
+    Represents a section for Daily Star i.e. Front Page, Sports etc
+    """
     def __init__(self, section_title,articles_raw,date):
+        """ 
+        Constructor
+        
+        @param {string} section_title - Title of the section
+        @param {[BeautifulSoup obj]} articles_raw - A list of BeautifulSoup obj. Each obj contains all the articles for a section
+        @param {string} date - Date of the articles
+        """
         self.section_title = section_title
         self.articles_raw = articles_raw
         self.date = date
         
-    def createArticles(self):
+    def writeArticlesToFile(self):
+        """ 
+        Extracts and transforms each article from BeautifulSoup to an Article obj, then writes the article text to file
+        """
         counter = 0
         for article in self.articles_raw:
             try:
@@ -40,41 +53,85 @@ class Section:
             
 
 class Article:
+    """
+    Represents a Daily Star article
+    """
     def __init__(self,section_title,article_title,article_content, date):
+        """ 
+        Constructor
+        
+         @param {string} section_title - Title of the section
+         @param {string} article_title - Title of the article
+         @param {BeautifulSoup obj} article_content - A BeautifulSoup obj that encapsulates the contents of the article
+         @param {string} date - Date of the articles
+        """
         self.section_title = section_title
         self.article_title = article_title
         self.article_content = article_content
         self.date = date
     
     def getSectionTitle(self):
+        """ 
+        Returns the section_title
+        """
         return self.section_title
     
     def getArticleTitle(self):
+        """ 
+        Returns the aritlce_title
+        """
         return self.article_title
     
     def getArticleContent(self):
+        """ 
+        Returns the article_content
+        """
         return self.article_content
     
     def extractText(self):
+        """
+        Extracts the articles from article_content and transforms it from BeautifulSoup obj to string
+        
+        @returns Article text in string format
+        """
         paras = self.article_content.find('div',attrs={'class':["field-body view-mode-full","field-body"]}).find_all('p')
         text = self._extractParasAndFormText(paras)
         return text
     
     def _extractParasAndFormText(self,paras):
+        """
+        Extracts the string text from BeautifulSoup paragraphs
+        
+        @param {BeautifulSoup obj} paras - Contains article text
+        @returns Article text in string format
+        """
         
         text = "\n ".join([str(para.text) for para in paras])
         return text
     
     def _createDirectories(self,dirPath):
+        """ 
+        Creates directories if they do not exist
+        
+        @param {string} dirPath - Directory path
+        """
         if not os.path.isdir(dirPath):
             os.makedirs(dirPath)
             
-    def _sanitizeText(self,title):
-        title = title.strip().lower()
-        textWithoutSpecialChar = re.sub("[.?!\"\n',]","",title)
+    def _sanitizeText(self,text):
+        """
+        Removes undesireable characters from the text so that they are valid file name.
+        
+        @param {string} text - tex tto be sanitized
+        """
+        text = text.strip().lower()
+        textWithoutSpecialChar = re.sub("[.?!\"\n',]","",text)
         return re.sub("[ ]","_",textWithoutSpecialChar)
             
-    def writeToFile(self):    
+    def writeToFile(self):
+        """ 
+        Write the article to a txt file.
+        """
         dirPath  = self._sanitizeText(f"{self.date}/{self.section_title}")
         self._createDirectories(dirPath)
     
@@ -89,7 +146,13 @@ class Article:
         
 ### HELPER METHODS
         
-def getContent(date):  
+def getContent(date):
+    """ 
+    Makes a GET request and gets html content back. 
+    
+    @param {string} date - Date for which we want the html content
+    @returns 
+    """
     address = "https://www.thedailystar.net/newspaper?date={}".format(date)
     
     #createDirectories(date)
@@ -101,6 +164,13 @@ def getContent(date):
     return content
 
 def findSectionsAndArticlesFromContent(date, content):
+    """ 
+    Finds and extracts all the news sections from the html content
+    
+    @param {string} date - String representation of the date for the sections
+    @param {BeautifulSoup obj} - BeautifulSoup obj that contains all the html content
+    @returns A dictionary mapping sections to their articles
+    """
     articlesInSection = {}
     
     for section in content:
@@ -111,6 +181,7 @@ def findSectionsAndArticlesFromContent(date, content):
     return articlesInSection
 
 def populateSections(date, articlesInSection):
+    """Returns a list of Section objects """
     sections = []
     for section_title, articles in list(articlesInSection.items()):
         sectionObj = Section(section_title,articles, date)
@@ -120,6 +191,7 @@ def populateSections(date, articlesInSection):
 
 
 def getContentAndTimeList(numOfDaysToScrape):
+    """Retrieves a list of content from Daily Star according to the number of days to scrape""" 
     content_list = []
     time_list = [] 
            
@@ -132,13 +204,16 @@ def getContentAndTimeList(numOfDaysToScrape):
 
 ### MAIN        
 def main():        
-    
     content_list, time_list = getContentAndTimeList(numOfDaysToScrape)
         
-    for i in range(len(content_list)): #O(3n^2)   
+    for i in range(len(content_list)):
+        start = pytime.time()
         articlesInSection = findSectionsAndArticlesFromContent(time_list[i],content_list[i])     
         sections = populateSections(time_list[i], articlesInSection)
-        [section.createArticles() for section in sections]
+        [section.writeArticlesToFile() for section in sections]
+        end = pytime.time()
+        delta = end - start
+        print(f"It took {delta} seconds to write to file.")
         
 if __name__ == "__main__":
     main()
